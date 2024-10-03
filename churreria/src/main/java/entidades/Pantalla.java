803e81dcc20e7dfbd5dce6b97c019f08bfbb2065
lusmax <lusmax0128@gmail.com>
2024-10-03 23:14:22 +0200

    crear metodos getTipoChurro, getSabor,getbebida en la clase Pantalla

--- churreria/src/main/java/entidades/Pantalla.java
+++ churreria/src/main/java/entidades/Pantalla.java
@@ -8,6 +8,9 @@ public abstract class Pantalla {
     public static final String COMPANY = "CHURRERÍA PRETTY WOMAN";
     public static final String SCRIPT = "Dulce tentación, perfecto en cada ocasión";
     public static final String ENCABEZADO_BIENVENIDA = "-PANTALLA DE BIENVENIDA-";
+    public static final String TIPO_CHURRO = "-Tipo churro:";
+    public static final String  SABOR = "-Sabores: ";
+    public static final String BEBIDA = "-Bebidas:";
     
     //Métodos
     
@@ -15,4 +18,17 @@ public abstract class Pantalla {
         String[] menu = {" 1. Ingresar Pedido", "2. Estado de Pedidos", "3.Entregar Pedidos", "4.Salir"};
         return menu;
     }
+    public static String[] getTipoChurro(){
+        String[] menu = {"1. Churro clásico", "2. Porra", "3.Relleno", "4. Especial"};
+        return menu;
+    }
+    public static String[] getSabor(){
+        String[] menu = {"1.Clásico", "2.Chocolate", "3.Chocolate blanco", "4.Vainilla", "5.Dulce de leche", "6.Avellanas", "7.Fresa", "8.Pistacho", "9.Oreo"};
+        return menu;
+    }
+    public static String[] getBebida(){
+        String[] menu = {"1.Agua ", "2.Limonada ", "3.Cola", "4.Zumo", "5.Té con limón", "6.Infusiones", "7.Agua con gas", "8.Suizo", "9.Cerveza"};
+        return menu;
+    } 
+    
 }
