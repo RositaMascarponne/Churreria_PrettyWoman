7b298e570d487804b31c13d27d501608e0b0c8f3
lusmax <lusmax0128@gmail.com>
2024-10-03 19:32:21 +0200

    crear proyecto churreria en netbeans y agregar dependencia mysql

---
+++ churreria/src/main/java/com/mycompany/churreria/Churreria.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.churreria;
+
+/**
+ *
+ * @author luisa
+ */
+public class Churreria {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
