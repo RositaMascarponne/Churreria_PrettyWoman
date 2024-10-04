d14f652f5ce1664f5d1ca1b6d72409d7bfdeb7aa
lusmax <lusmax0128@gmail.com>
2024-10-04 12:03:47 +0200

    crear javabeans de la clase Pedido

---
+++ churreria/src/main/java/entidades/Pedido.java
@@ -0,0 +1,66 @@
+package entidades;
+
+/**
+ *
+ * @author luisa
+ */
+public class Pedido {
+
+    //Atributos de la clase Pedido
+    private int pedidoId;
+    private int churroId;
+    private int bebidaId;
+    private int estado_Id;
+
+    //Constructores
+    public Pedido() {
+    }
+
+    public Pedido(int pedidoId, int churroId, int bebidaId, int estado_Id) {
+        this.pedidoId = pedidoId;
+        this.churroId = churroId;
+        this.bebidaId = bebidaId;
+        this.estado_Id = estado_Id;
+    }
+
+    //Getters
+    public int getPedidoId() {
+        return pedidoId;
+    }
+
+    public int getChurroId() {
+        return churroId;
+    }
+
+    public int getBebidaId() {
+        return bebidaId;
+    }
+
+    public int getEstado_Id() {
+        return estado_Id;
+    }
+//Setters
+
+    public void setPedidoId(int pedidoId) {
+        this.pedidoId = pedidoId;
+    }
+
+    public void setChurroId(int churroId) {
+        this.churroId = churroId;
+    }
+
+    public void setBebidaId(int bebidaId) {
+        this.bebidaId = bebidaId;
+    }
+
+    public void setEstado_Id(int estado_Id) {
+        this.estado_Id = estado_Id;
+    }
+//Métodos 
+
+    @Override
+    public String toString() {
+        return "Pedido{" + "pedidoId=" + pedidoId + ", churroId=" + churroId + ", bebidaId=" + bebidaId + ", estado_Id=" + estado_Id + '}';
+    }
+
+}
