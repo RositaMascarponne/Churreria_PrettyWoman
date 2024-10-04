2621ea5aa2394274fb46c8a2a12605fe79ed6b67
RositaMascarponne <rtirado.crn@artsgrafiques.org>
2024-10-04 10:31:45 +0200

    Clase churros: Completados atributos y añadidos setters

--- churreria/src/main/java/entidades/Churro.java
+++ churreria/src/main/java/entidades/Churro.java
@@ -1,37 +1,77 @@
 package entidades;
+
 /**
  *
  * @author luisa
  */
 public class Churro {
+
     //Atributos de la clase churro
     private int churroId;
     private String churroNombre;
     private String churroPrecio;
-    
+    private int tipoId;
+    private int saborId;
+
     //Constructores
-    public Churro(){
-        
+    public Churro() {
+
     }
-    
-   public Churro(String churroNombre, String churroPrecio){
-       this.churroNombre = churroNombre;
-       this.churroPrecio = churroPrecio;
-   }
+
+    public Churro(int churroId, String churroNombre, String churroPrecio, int tipoId, int saborId) {
+        this.churroId = churroId;
+        this.churroNombre = churroNombre;
+        this.churroPrecio = churroPrecio;
+        this.tipoId = tipoId;
+        this.saborId = saborId;
+    }
     //Getters
-   public int getChurroId(){
-       return this.churroId;
-   }
-   public String getChurroNombre(){
-       return this.churroNombre;            
-   }
-   public String getChurroPrecio(){
-       return this.churroPrecio;
-   }
-   //Métodos
-   @Override
-   public String toString(){
-      return "Churro{" + "churroId=" + churroId + ",churroNombre=" + churroNombre + ",churroPrecio=" + churroPrecio + '}';
-   }
-        
+
+    public int getChurroId() {
+        return this.churroId;
+    }
+
+    public String getChurroNombre() {
+        return this.churroNombre;
+    }
+
+    public String getChurroPrecio() {
+        return this.churroPrecio;
+    }
+
+    public int getTipoId() {
+        return this.tipoId;
+    }
+
+    public int getSaborId() {
+        return this.saborId;
+    }
+
+    //Setters
+    public void setSaborID(int saborId) {
+        this.saborId = saborId;
+    }
+
+    public void setChurroNombre(String churroNombre) {
+        this.churroNombre = churroNombre;
+    }
+
+    public void setChurroPrecio(String churroPrecio) {
+        this.churroPrecio = churroPrecio;
+    }
+
+    public void setTipoId(int tipoId) {
+        this.tipoId = tipoId;
+    }
+
+    public void setSaborId(int saborId) {
+        this.saborId = saborId;
+    }
+
+    //Métodos
+    @Override
+    public String toString() {
+        return "Churro{" + "churroId=" + churroId + ",churroNombre=" + churroNombre + ",churroPrecio=" + churroPrecio + '}';
+    }
+
 }
