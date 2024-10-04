799ddc018865214a2c92ed2080ba4b4e17b26e36
RositaMascarponne <rtirado.crn@artsgrafiques.org>
2024-10-04 13:11:15 +0200

    Conexion establecida java-base de datos

--- churreria/src/main/java/com/mycompany/churreria/Churreria.java
+++ churreria/src/main/java/com/mycompany/churreria/Churreria.java
@@ -4,6 +4,12 @@
 
 package com.mycompany.churreria;
 
+import entidades.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 /**
  *
  * @author luisa
@@ -11,6 +17,22 @@ package com.mycompany.churreria;
 public class Churreria {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+                try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            String URL = "jdbc:mysql://localhost:3308/churreria_pretty_woman";
+            Connection conn = DriverManager.getConnection(URL, "root", "root");
+            /*
+            Menu menu = new Menu(conn);
+            menu.mostrarMenu();
+            */
+                    Churro churrito=new Churro();
+                    System.out.println(churrito);
+            conn.close();
+            System.out.println("Conexión cerrada");
+
+        } catch (SQLException | ClassNotFoundException e) {
+            e.printStackTrace();
+            System.out.println("Se ha producido un error en la conexión.");
+        }
     }
 }
