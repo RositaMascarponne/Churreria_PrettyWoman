3f8e69aef776d20d7eda3c6655d0f3698585f962
RositaMascarponne <rtirado.crn@artsgrafiques.org>
2024-10-05 15:16:19 +0200

    Creada una clase para el supermenu de Luisa, ahora se puede instanciar. Tambien creado un nuevo package dedicado a las clases logicas, por un lado tenemos un package para entidades y pot otro uno dedicado a menu, pantallas, etc

--- churreria/src/main/java/com/mycompany/churreria/Churreria.java
+++ churreria/src/main/java/com/mycompany/churreria/Churreria.java
@@ -4,6 +4,7 @@
 package com.mycompany.churreria;
 
 import entidades.*;
+import logica.*;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
@@ -24,35 +25,8 @@ public class Churreria {
             Connection conn = DriverManager.getConnection(URL, "root", "root");
 
             //Mostrando Menú en pantalla
-            String company = String.format("\n%80s\n", "------------------" + Pantalla.COMPANY + "-------------------");
-            System.out.print(company);
-
-            String script = String.format("%70s", Pantalla.SCRIPT);
-            System.out.println(script);
-
-            System.out.println("                     ----------------------------------------------------------- ");
-            String pBienvenida = String.format("\n%60s\n", Pantalla.ENCABEZADO_BIENVENIDA);
-            System.out.println(pBienvenida);
-            System.out.println("");
-
-            // Mostrando opciones del Menú
-            String[] menu = Pantalla.getMenuBienvenida();
-            String menuFormateado;
-            for (int i = 0; i < menu.length; i++) {
-                menuFormateado = String.format("%30s", menu[i]);
-                switch (i) {
-                    case 2 -> {
-                        System.out.println(menuFormateado);
-                        System.out.println("           --------------------------------------------------------------------------------\n ");
-                    }
-                    case 3 -> {
-                        menuFormateado = String.format("%53s", menu[i]);
-                        System.out.print(menuFormateado);
-                    }
-                    default -> System.out.print(menuFormateado);
-                }
-            }
-            System.out.println("");
+            PantallaBienvenida pantallaBienvenida = new PantallaBienvenida();
+            pantallaBienvenida.mostrar();
 
             //CIERRE CONEXIÓN A BASE DE DATOS        
             conn.close();
---
+++ churreria/src/main/java/logica/PantallaBienvenida.java
@@ -0,0 +1,53 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package logica;
+
+import entidades.*;
+import java.util.Scanner;
+
+/**
+ *
+ * @author Rosita
+ */
+public class PantallaBienvenida {
+        public void mostrar() {
+        // Mostrando Menú en pantalla
+        String company = String.format("\n%80s\n", "------------------" + Pantalla.COMPANY + "-------------------");
+        System.out.print(company);
+
+        String script = String.format("%70s", Pantalla.SCRIPT);
+        System.out.println(script);
+
+        System.out.println("                     ----------------------------------------------------------- ");
+        String pBienvenida = String.format("\n%60s\n", Pantalla.ENCABEZADO_BIENVENIDA);
+        System.out.println(pBienvenida);
+        System.out.println("");
+
+        // Mostrando opciones del Menú
+        String[] menu = Pantalla.getMenuBienvenida();
+        String menuFormateado;
+
+        for (int i = 0; i < menu.length; i++) {
+            menuFormateado = String.format("%30s", menu[i]);
+            switch (i) {
+                case 2 -> {
+                    System.out.println(menuFormateado);
+                    System.out.println("           --------------------------------------------------------------------------------\n ");
+                }
+                case 3 -> {
+                    menuFormateado = String.format("%53s", menu[i]);
+                    System.out.print(menuFormateado);
+                }
+                default -> System.out.print(menuFormateado);
+            }
+        }
+        System.out.println("");
+
+        // Leer la opción del usuario
+    
+    }
+
+
+}
