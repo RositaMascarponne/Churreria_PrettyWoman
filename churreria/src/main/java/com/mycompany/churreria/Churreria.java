73e89d818329ce25f35ce8f392ca2281ed025cd8
lusmax <lusmax0128@gmail.com>
2024-10-08 13:01:03 +0200

    borrar paquete PantallaPedido y eliminar contenido de Churreria

--- churreria/src/main/java/com/mycompany/churreria/Churreria.java
+++ churreria/src/main/java/com/mycompany/churreria/Churreria.java
@@ -12,36 +12,6 @@ package com.mycompany.churreria;
 public class Churreria {
 
     public static void main(String[] args) {
-<<<<<<< HEAD
 
-=======
-        //CONEXIÓN A LA BASE DE DATOS
-        try {
-           
-            String URL = "jdbc:mysql://localhost:3306/churreria_pretty_woman";
-            Connection conn = DriverManager.getConnection(URL, "root", "root");
-
-            // Mostrando Menú en pantalla
-            //Mostrando Menú en pantalla
-            PantallaBienvenida pantallaBienvenida = new PantallaBienvenida();
-            pantallaBienvenida.mostrar();
-
-           Menu menuPrincipal = new Menu();
-            menuPrincipal.mostrarMenuPrincipal();
-
-            //Traer info de la base de dato
-            Churro miChurro = new Churro();
-            float precio = miChurro.getPrecioUnitario();
-            System.out.println(precio);
-
-            //CIERRE CONEXIÓN A BASE DE DATOS        
-            conn.close();
-            System.out.println("Conexión cerrada");
-
-        } catch (SQLException e) {
-            e.printStackTrace();
-            //System.out.println("Se ha producido un error en la conexión. Puede que no estes en el puerto correcto eh? ¿Pero lo has cambiado?¿Seguro eh? ");
-        }
->>>>>>> fa1adc5fc69c53d787e38e96409d7283fd56635e
     }
 }
