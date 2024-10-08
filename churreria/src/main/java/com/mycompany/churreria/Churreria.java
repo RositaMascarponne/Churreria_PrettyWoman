977734fe8eba24304d680190152c4e09f0b7b285
RositaMascarponne <rtirado.crn@artsgrafiques.org>
2024-10-08 12:39:37 +0200

    Adios a dias y horas de arduo trabajo, hay que rehacerlo mas simple

--- churreria/src/main/java/com/mycompany/churreria/Churreria.java
+++ churreria/src/main/java/com/mycompany/churreria/Churreria.java
@@ -3,14 +3,7 @@
  */
 package com.mycompany.churreria;
 
-import Pantallas.*;
-import entidades.*;
-import logica.*;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+
 
 /**
  *
@@ -19,34 +12,6 @@ import java.sql.Statement;
 public class Churreria {
 
     public static void main(String[] args) {
-        //CONEXIÓN A LA BASE DE DATOS
-        try {
-            Class.forName("com.mysql.cj.jdbc.Driver");
-            String URL = "jdbc:mysql://localhost:3306/churreria_pretty_woman";
-            Connection conn = DriverManager.getConnection(URL, "root", "root");
-            
-                    // Mostrando Menú en pantalla
 
-                    
-
-
-            //Mostrando Menú en pantalla
-            PantallaBienvenida pantallaBienvenida= new PantallaBienvenida();
-            pantallaBienvenida.mostrar();
-            
-            Menu menuPrincipal=new Menu();
-            menuPrincipal.mostrarMenuPrincipal();
-            
-            
-            
-
-            //CIERRE CONEXIÓN A BASE DE DATOS        
-            conn.close();
-            System.out.println("Conexión cerrada");
-
-        } catch (SQLException | ClassNotFoundException e) {
-            e.printStackTrace();
-            System.out.println("Se ha producido un error en la conexión. Puede que no estes en el puerto correcto eh? ¿Pero lo has cambiado?¿Seguro eh? ");
-        }
     }
 }
--- churreria/src/main/java/entidades/Churro.java
+++ churreria/src/main/java/entidades/Churro.java
@@ -4,6 +4,7 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -13,87 +14,26 @@ import java.util.List;
  */
 public class Churro {
 
-    //Atributos de la clase churro
-    private int churroId;
-    private String churroNombre;
-    private int churroCantidad;
-    private float churroPrecio;
-    private float precioUnitario;
+    private Connection conn;
 
-    //Constructores
-    public Churro() {
-
+    public Churro(Connection conn) {
+        this.conn = conn;
     }
 
-    public Churro(int churroId, String churroNombre, int churroCantidad, float churroPrecio, float precioUnitario) {
-        this.churroId = churroId;
-        this.churroNombre = churroNombre;
-        this.churroPrecio = churroPrecio;
-        this.churroCantidad = churroCantidad;
-        this.precioUnitario = precioUnitario;
-
-    }
-    //Getters
-
-    public int getChurroId() {
-        return this.churroId;
-    }
-
-    public String getChurroNombre() {
-        return this.churroNombre;
-    }
-
-    public float getChurroPrecio() {
-        return this.churroPrecio;
-    }
-    
-    public int getChurroCantidad(){
-        return this.churroCantidad;
-    }
-
-    public void setChurroId(int churroId) {
-        this.churroId = churroId;
-    }
-
-    public void setChurroNombre(String churroNombre) {
-        this.churroNombre = churroNombre;
-    }
-
-    public void setChurroPrecio(float churroPrecio) {
-        this.churroPrecio = churroPrecio;
-    }
-
-    public float getPrecioUnitario() {
-        return precioUnitario;
-    }
-
-    //Métodos
-    public float getPrecioTotal() {
-        return churroCantidad * precioUnitario;
-    }
-    
-    // Método para cargar churros desde la base de datos
-        public void cargarDesdeBaseDeDatos(Connection conn, int id) throws SQLException {
-        String query = "SELECT churroNombre, churroCantidad, churroPrecio FROM churros WHERE id = 1";
-        PreparedStatement stmt = conn.prepareStatement(query);
-        stmt.setInt(1, id);
-        ResultSet rs = stmt.executeQuery();
-        
-        if (rs.next()) {
-            // Asignar valores desde la base de datos a los atributos
-            this.churroNombre = rs.getString("nombre");
-            this.churroCantidad = rs.getInt("cantidad");
-            this.precioUnitario = rs.getFloat("precio_unitario");
-            this.churroPrecio = this.churroCantidad * this.precioUnitario;
+    public void mostrarChurro() throws SQLException {
+        String sql = "SELECT * FROM churros";
+        Statement st = conn.createStatement();
+        ResultSet rs = st.executeQuery(sql);
+        while (rs.next()) {
+            System.out.println(rs.getInt("churroID") + ": " + rs.getString("churroNombre"));
         }
-
-        rs.close();
-        stmt.close();
     }
 
-    @Override
-    public String toString() {
-        return "Churro{" + "churroId=" + churroId + ",churroNombre=" + churroNombre + ",churroPrecio=" + churroPrecio + '}';
+    public void agregarChurro(String nombre) throws SQLException {
+        String sqlInsert = "INSERT INTO churros(churroNombre) VALUES('" + nombre + "')";
+        Statement st = conn.createStatement();
+        st.executeUpdate(sqlInsert);
+        System.out.println("Churro agregado: " + nombre);
     }
 
 }
